c6a4d174b29f36e85b90a239ce0155c6b61fa78d
Ryan Nardelli <ryannardelli12@gmail.com>
2024-06-22 18:06:47 -0300

    criei o slider de feedback dos clientes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// SLIDER TOP 
 document.addEventListener('DOMContentLoaded', () => {
     const list = document.querySelector('.slider .list');
     const items = Array.from(list.children);
@@ -7,3 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
         list.appendChild(clone);
     });
 });
+
+// SWIPER SLIDER
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+    },
+    loop: true,
+  });
